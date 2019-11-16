e03df4ecc81e929acc365087ddfee2d87dc63dba
domotord90 <jazzmaster@freemail.com>
2019-11-16 13:07:22 +0100

    Updates

---
+++ precache-manifest.9e843716d3118c4d49b895f98b17a6fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d147085fed7b030bf79a5c66a09ad60",
+    "url": "/Book-Wiz/index.html"
+  },
+  {
+    "revision": "3ba2dc7855abe10f3f8d",
+    "url": "/Book-Wiz/static/css/main.d704009e.chunk.css"
+  },
+  {
+    "revision": "6ba744eab47faf2ae0aa",
+    "url": "/Book-Wiz/static/js/2.721440a5.chunk.js"
+  },
+  {
+    "revision": "3ba2dc7855abe10f3f8d",
+    "url": "/Book-Wiz/static/js/main.7da9c711.chunk.js"
+  },
+  {
+    "revision": "65e9693b2bbb150e47b8",
+    "url": "/Book-Wiz/static/js/runtime-main.5ef5f87c.js"
+  },
+  {
+    "revision": "e9eb705a754a6ede8808f82f140baf15",
+    "url": "/Book-Wiz/static/media/placeholder.e9eb705a.jpg"
+  }
+]);
